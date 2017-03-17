40505c6f2aa5bb456a9100b5c50f1b0196be5acc
Alessandro Gargiulo <alej.gargiulo@gmail.com>
2017-03-17 23:46:01 +0100

    Add log4net support

--- src/MusicInside/Startup.cs
+++ src/MusicInside/Startup.cs
@@ -14,11 +14,14 @@ using MusicInside.ManagerInterfaces;
 using MusicInside.DataAccessInterfaces;
 using MusicInside.DataAccess;
 using MusicInside.Business;
+using System.Xml;
+using System.Reflection;
 
 namespace MusicInside
 {
     public class Startup
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(Program));
         public Startup(IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
@@ -27,6 +30,15 @@ namespace MusicInside
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
             Configuration = builder.Build();
+
+            // Log4net Configuration
+            XmlDocument log4netConfig = new XmlDocument();
+            log4netConfig.Load(System.IO.File.OpenRead("log4net.config"));
+
+            var repo = log4net.LogManager.CreateRepository(Assembly.GetEntryAssembly(), typeof(log4net.Repository.Hierarchy.Hierarchy));
+
+            log4net.Config.XmlConfigurator.Configure(repo, log4netConfig["log4net"]);
+            log.Info("Music Inside Web App | Application Started.");
         }
 
         public IConfigurationRoot Configuration { get; }
@@ -54,7 +66,7 @@ namespace MusicInside
             services.AddScoped(typeof(IArtistManager), typeof(ArtistManager));
             services.AddScoped(typeof(IAlbumManager), typeof(AlbumManager));
         }
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, SongDBContext context)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
@@ -80,8 +92,6 @@ namespace MusicInside
             });
 
             DbInitializer.Initialize(context);
-
-            //log4net.Config.XmlConfigurator.Configure();
         }
     }
 }
