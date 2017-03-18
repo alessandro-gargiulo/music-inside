093c6242cb3fa6db556277c5480e410fc5bfd28f
Alessandro Gargiulo <alej.gargiulo@gmail.com>
2017-03-18 19:59:37 +0100

    Add DI for statistic data access

--- src/MusicInside/Startup.cs
+++ src/MusicInside/Startup.cs
@@ -64,6 +64,7 @@ namespace MusicInside
             services.AddScoped(typeof(IArtistDataAccess), typeof(ArtistDataAccess));
             services.AddScoped(typeof(IAlbumDataAccess), typeof(AlbumDataAccess));
             services.AddScoped(typeof(IGenreDataAccess), typeof(GenreDataAccess));
+            services.AddScoped(typeof(IStatisticDataAccess), typeof(StatisticDataAccess));
             // Business Layer
             services.AddScoped(typeof(ISongManager), typeof(SongManager));
             services.AddScoped(typeof(IArtistManager), typeof(ArtistManager));
