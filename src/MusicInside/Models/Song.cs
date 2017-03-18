e23f4104c785d3c8dc00213712ac035b4f6a1189
Alessandro Gargiulo <alej.gargiulo@gmail.com>
2017-03-18 18:12:14 +0100

    Album ID is mandatory

--- src/MusicInside/Models/Song.cs
+++ src/MusicInside/Models/Song.cs
@@ -11,7 +11,7 @@ namespace MusicInside.Models
         public string Title { get; set; }
         public int TrackNo { get; set; }
         public int Year { get; set; }
-        public int? AlbumId { get; set; }
+        public int AlbumId { get; set; }
         public Album Album { get; set; }
         public File File { get; set; }
         public Statistic Statistic { get; set; }
