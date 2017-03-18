b54ff07bfdb40f63420e26613d692f1ba022440e
Alessandro Gargiulo <alej.gargiulo@gmail.com>
2017-03-18 18:19:02 +0100

    Add trackNumber to model

--- src/MusicInside/ModelView/SongDetailViewModel.cs
+++ src/MusicInside/ModelView/SongDetailViewModel.cs
@@ -9,8 +9,9 @@ namespace MusicInside.ModelView
     {
         public string ArtistLabel { get; set; }
         public string TitleLabel { get; set; }
-        public string AlbumLable { get; set; }
+        public string AlbumLabel { get; set; }
         public int Year { get; set; }
+        public int TrackNo { get; set; }
         public string GenreLabel { get; set; }
         public DateTime LastPlay { get; set; }
         public int NumOfPlays { get; set; }
