857fcdc9f4c156ba1d5397339550bdfc908a9a65
Alessandro Gargiulo <alej.gargiulo@gmail.com>
2017-03-18 18:03:52 +0100

    Add Exception classes.

---
+++ src/MusicInside/Exceptions/InvalidSongIdException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MusicInside.Exceptions
+{
+    [Serializable]
+    internal class InvalidIdException : Exception
+    {
+        public InvalidIdException()
+        {
+        }
+
+        public InvalidIdException(string message) : base(message)
+        {
+        }
+
+        public InvalidIdException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ src/MusicInside/Exceptions/SongNotPresentException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MusicInside.Exceptions
+{
+    [Serializable]
+    internal class EntryNotPresentException : Exception
+    {
+        public EntryNotPresentException()
+        {
+        }
+
+        public EntryNotPresentException(string message) : base(message)
+        {
+        }
+
+        public EntryNotPresentException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
