c25c2dbf1ea43058cc68a428d5110fd4f6a3c2ea
Alessandro Gargiulo <alej.gargiulo@gmail.com>
2017-03-22 23:44:43 +0100

    Add MusicUpdateBatch

---
+++ MusicUpdateBatch/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MusicUpdateBatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // Dependency Injection for Logger and MusicInside Database Classes
+            // Read JSON configuration file and get the path of music folder.
+        }
+    }
+}
