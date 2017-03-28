a587d685b00fa71d0ecb132df9a182e0eec40be7
Alessandro Gargiulo <alej.gargiulo@gmail.com>
2017-03-29 00:49:14 +0200

    First DI test in Console App

--- MusicUpdateBatch/Program.cs
+++ MusicUpdateBatch/Program.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MusicInside.Models;
 using System;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Xml;
@@ -12,7 +15,8 @@ namespace MusicUpdateBatch
     {
         private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(Program));
         private static IConfigurationRoot Configuration;
-        
+        private static readonly string APPFOLDERNAME = "MusicUpdateBatch";
+
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
@@ -23,25 +27,20 @@ namespace MusicUpdateBatch
             Console.WriteLine("--------Author: Alessandro Gargiulo      --------");
             Console.WriteLine("-------------------------------------------------");
 
-            string environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-
-            if (String.IsNullOrWhiteSpace(environment))
-                throw new ArgumentNullException("Environment not found in ASPNETCORE_ENVIRONMENT");
-
-            Console.WriteLine("Environment: {0}", environment);
-
             var services = new ServiceCollection();
 
+            string BasePath = Directory.GetParent(Directory.GetCurrentDirectory()).FullName;
             // Set up fon configuration sources
             var builder = new ConfigurationBuilder()
-                .SetBasePath(Path.Combine(AppContext.BaseDirectory))
-                .AddJsonFile("appsettings.json", optional: true);
+                .SetBasePath(Path.Combine(BasePath, APPFOLDERNAME))
+                .AddJsonFile("appsettings.json", optional: true)
+                .AddEnvironmentVariables();
 
             Configuration = builder.Build();
 
             // Log4net Configuration
             XmlDocument log4netConfig = new XmlDocument();
-            log4netConfig.Load(System.IO.File.OpenRead("log4net.config"));
+            log4netConfig.Load(System.IO.File.OpenRead("log4net.xml"));
 
             var repo = log4net.LogManager.CreateRepository(Assembly.GetEntryAssembly(), typeof(log4net.Repository.Hierarchy.Hierarchy));
 
@@ -51,14 +50,32 @@ namespace MusicUpdateBatch
             services.AddSingleton<IConfiguration>(Configuration);
             // Dependency injection for Log
             services.AddSingleton<log4net.ILog>(log);
+            // Dependency injection for DBContext
+            services.AddDbContext<SongDBContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("MusicInsideDatabase")));
+
+            services.AddTransient<Prova>();
+
+            var provider = services.BuildServiceProvider();
+
+            using(var prova = provider.GetService<Prova>())
+            {
+
+            }
         }
     }
 
-    public static class ServicesExtension
+    public class Prova : IDisposable
     {
-        public static IServiceCollection AddMusicInside(this IServiceCollection services)
+        public Prova(log4net.ILog log, SongDBContext context)
         {
-            return services;
+            log.Info("Ci sono riuscito");
+            log.Info(context.Songs.Where(x => x.Title == "Albachiara").Select(y => y.Year));
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
         }
     }
 }
